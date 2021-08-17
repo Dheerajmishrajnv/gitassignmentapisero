34c9d445abdff32d35dcf5371eb21c71119d6d35
Dheeraj Mishra <bulbulmishrajnv@gmail.com>
2021-08-17 11:52:49 +0530

    Changes done with the .gitignore

--- src/gitdemo/Hello.java
+++ src/gitdemo/Hello.java
@@ -3,7 +3,7 @@ package gitdemo;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, this is my first git push");
+		System.out.println("Hello, this is my first git work");
 	}
 
 }
